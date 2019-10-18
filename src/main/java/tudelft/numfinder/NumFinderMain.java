1b79ea6e00fec4aa612a0eaec68114f189632e35
Sergey Buta <sergey.buta@gmail.com>
2019-10-18 20:26:35 +0300

    Complete NumFinder exercise

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -9,9 +9,9 @@ public class NumFinderMain {
         // nf.find(new int[] {4, 25, 7, 9});
 
         // this crashes
-        // snf.find(new int[] {4, 3, 2, 1});
+        // nf.find(new int[] {4, 3, 2, 1});
         
-        nf.find(null);
+        nf.find(new int[] {4, 3, 2, 1});
 
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
